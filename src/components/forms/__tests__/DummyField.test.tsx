b45071e5a7bd41bb53a3c423d0fedf4ddce9c9fc
Mohd Khairi Mohd Yusoff <mkhairiyusoff.1985@gmail.com>
2025-07-03 01:57:55 +0800

    test: add dummy test for CI verification

---
+++ src/components/forms/__tests__/DummyField.test.tsx
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+
+function DummyField() {
+  return <div data-testid="dummy-field">Hello Dummy</div>;
+}
+
+describe("DummyField", () => {
+  it("renders the dummy text", () => {
+    render(<DummyField />);
+    expect(screen.getByTestId("dummy-field")).toHaveTextContent("Hello Dummy");
+  });
+});
