198a7241ce1b0569f113f54c04de5fc5370d0f7c
Mohd Khairi Mohd Yusoff <mkhairiyusoff.1985@gmail.com>
2026-04-18 19:17:27 +0800

    refactor(accounts): add pages barrel

---
+++ src/features/accounts/pages/index.ts
@@ -0,0 +1,3 @@
+export { default as AccountDetailsPage } from "./AccountDetailsPage";
+export { default as DepositPage } from "./DepositPage";
+export { default as WithdrawPage } from "./WithdrawPage";
