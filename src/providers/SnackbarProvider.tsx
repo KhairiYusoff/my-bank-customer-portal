1e0c05a4bf833d079d6723d913549d9b9a1ceba6
Mohd Khairi Mohd Yusoff <mkhairiyusoff.1985@gmail.com>
2026-05-11 18:41:50 +0800

    fix: add proper spacing for toast bar

--- src/providers/SnackbarProvider.tsx
+++ src/providers/SnackbarProvider.tsx
@@ -2,6 +2,7 @@ import { useTheme } from "@mui/material";
 import { SnackbarProvider as NotistackSnackbarProvider } from "notistack";
 import { ReactNode } from "react";
 import { SnackbarUtilsConfigurator } from "@/utils/snackbarUtils";
+import { useAppSelector } from "@/app/hooks";
 
 interface SnackbarProviderProps {
   children: ReactNode;
@@ -9,6 +10,7 @@ interface SnackbarProviderProps {
 
 export const SnackbarProvider = ({ children }: SnackbarProviderProps) => {
   const theme = useTheme();
+  const { isAuthenticated } = useAppSelector((state) => state.auth);
 
   return (
     <NotistackSnackbarProvider
@@ -16,6 +18,7 @@ export const SnackbarProvider = ({ children }: SnackbarProviderProps) => {
       autoHideDuration={5000}
       anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
       style={{
+        marginTop: isAuthenticated ? '20px' : '60px',
         '--SnackbarItem-variantSuccessBackground': theme.palette.success.main,
         '--SnackbarItem-variantErrorBackground': theme.palette.error.main,
       } as React.CSSProperties}
